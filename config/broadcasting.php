10b4170494378f18721bd150d9ae556f6c12d0a5
Aeron <senciljohnaeron@gmail.com>
2023-11-26 14:16:19 +0800

    realtime notification is now fixed!

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -29,19 +29,22 @@ return [
     */
 
     'connections' => [
-
+        'pusher-custom' => [
+            'driver' => 'pusher-custom'
+        ],
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                'host' => 'api-'.env('PUSHER_APP_CLUSTER').'.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'useTLS' => true,
+                // 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https'
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
